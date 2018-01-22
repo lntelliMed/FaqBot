dd265eee6725038e05c5409d04a34edb5ff8ee25
“Sam <sam.alsmadi@gmail.com>
2018-01-21 21:47:03 -0500

    Update seed file contents

--- client/components/main.js
+++ client/components/main.js
@@ -14,9 +14,25 @@ class Main extends Component {
 
     this.handleTextChange = this.handleTextChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+
+
   }
 
+  // componentWillReceiveProps(nextProps) {
+  //   if (nextProps.myProp !== this.props.myProps) {
+  //     // nextProps.myProp has a different value than our current prop
+  //     // so we can perform some calculations based on the new value
+  //     scrollToBottom('scrollable-list')
+  //   }
+  // }
 
+  // componentWillUpdate(nextProps, nextState) {
+    // if (nextState.open == true && this.state.open == false) {
+    //   this.props.onWillOpen();
+    // }
+    // scrollToBottom('scrollable-list')
+    // document.getElementById('scrollable-list').scrollIntoView(false);
+  // }
 
 
   // TO-DO: Check if there is a need for  props will change hook in case same message entered twice
@@ -37,6 +53,13 @@ class Main extends Component {
     this.setState({
       enteredMessage: ''
     })
+
+    // document.getElementById('scrollable-list').scrollIntoView(false);
+    try{
+      scrollToBottom('scrollable-list')
+    } catch (err){
+      console.error(err)
+    }
   }
 
 
@@ -51,13 +74,15 @@ render() {
       </div>
 
       <Form onSubmit={this.handleSubmit}>
-        <ul className='chat-window'>
+        <ul className='chat-window' id='scrollable-list'>
           {this.props.messages.map((message, index) => {
             return (
-              <div  key={index}>
+              <div className='switch-direction' key={index}>
+                {/* <Image className={index % 2 === 0 ? 'chat-picture' : 'switch-picture-position'} bordered circular src={index % 2 === 0 ? './guest.png' : './bot.png'} /> */}
                 <Image className='chat-picture' bordered circular src={index % 2 === 0 ? './guest.png' : './bot.png'} />
-                {/* <div className='chat-bubble' > {message} </div> */}
-                <Form.Input className='chat-bubble' placeholder='Type a message..' value={message} />
+                <div className='chat-bubble' > {message} </div>
+                {/* <li className='chat-bubble' > {message} </li> */}
+                {/* <Form.Input className='chat-bubble' placeholder='Type a message..' value={message} /> */}
               </div>
             )
           })}
@@ -101,6 +126,10 @@ function startDictation() {
   }
 }
 
+function scrollToBottom(id) {
+  var div = document.getElementById(id);
+  div.scrollTop = div.scrollHeight - div.clientHeight;
+}
 
 const mapState = (state) => {
   return {
--- script/seed.js
+++ script/seed.js
@@ -8,12 +8,18 @@ async function seed () {
 
 
   const messages = await Promise.all([
-    Message.create({ intent: 'greeting', reply: ['Hey there!', 'How is it going?'] }),
+    Message.create({ intent: 'greeting', reply: ['Hey there!', 'Hey, how is it going?'] }),
+    Message.create({ intent: 'goodbye', reply: ['Bye bye!', 'Take care my friend!', 'See you later!'] }),
     Message.create({ intent: 'error', reply: ['Sorry, did not understand what you mean!', 'Not sure I understood you correctly'] }),
-    Message.create({ intent: 'undefined_faq', reply: ['So, you are asking about undefined. That is a fantastic question'] }),
+    Message.create({ intent: 'undefined_null', reply: [`So, you are asking about undefined versus null. That is a fantastic question. null is opt -in, undefined is often a "default" value, for example: for declared variables, the return value of a function that has no explicit return statement, object properties that don't exist. That means null is a signal to anybody seeing it that the programmer CHOSE to return null.Similarly it can reference something that currently has "no value" but will, or can in some cases. Sometimes null can be chosen for math(for math operations, it works like 0)`] }),
     Message.create({
       intent: 'react_state', reply: [`In react, state is a way to store data that is used in our view AND CHANGES. When we call setState it will change the data AND trigger react to re-render the virtual DOM for the component that was setStated.`]}),
-    Message.create({ intent: 'pure_function', reply: ['It means no side effects', 'The function takes input and returns output AND DOES NOTHING ELSE. For example, it does not console.log or query a database or send Nimit text message.'] }),
+    Message.create({ intent: 'pure_function', reply: ['It means no side effects', 'A pure function takes input and returns output AND DOES NOTHING ELSE. For example, it does not console.log or query a database or send Nimit text message.'] }),
+    Message.create({
+      intent: 'use_strict', reply: [`It constrains your program in certain useful ways, called strict mode. For example, the keyword this defaults to undefined instead of the global object`, `It forbids (I think) accidental global assignment (x = 5 as opposed to var x = 5)`, `It forbids octal syntax`, `It forbids with and a lot of other stuff. I think ES6 interpreters default to strict mode.`] }),
+    Message.create({
+      intent: 'garbage_collection', reply: [`Javascript interpreters routinely garbage collects any data for which no existing variable points to it. A variable can also point to some data by being an object or array, and having that data as a property value or element.`] }),
+
 
   ])
 
